a46d783ab83d31090a40b1dcc779b51a3ade4739
Niv Zohar <nivzohar@microsoft.com>
2023-05-28 20:12:56 +0300

    update html file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-    base: "/vectory/",
+    base: "/Vectory/",
     plugins: [svelte()],
 })
